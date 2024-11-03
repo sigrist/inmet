8116221ec7a98261b2b94bd58b7c4965d43c2472
Paulo Sigrist <paulo.sigrist@gmail.com>
2024-11-03 10:57:27 -0300

    Implementado busca de cidade no assistente (#5)
    close #4

--- custom_components/inmet/const.py
+++ custom_components/inmet/const.py
@@ -13,4 +13,8 @@ FEED = "feed"
 DEFAULT_ICON = "mdi:check"
 ALERT_ICON = "mdi:alert"
 DEFAULT_CODE = "3509502"  # Campinas
+DEFAULT_NAME = "Campinas"  # Campinas
 DEFAULT_SCAN_INTERVAL = timedelta(minutes=30)
+
+# Definindo o número máximo de cidades a exibir
+MAX_CITIES = 5
