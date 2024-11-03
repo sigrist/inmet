03d90ec9b920a96d1fb1eba9ad5c1524498139af
Paulo Sigrist <paulo.sigrist@gmail.com>
2024-11-03 13:55:55 +0000

    Implementado busca de cidade no assistente

--- custom_components/inmet/config_flow.py
+++ custom_components/inmet/config_flow.py
@@ -1,4 +1,4 @@
-"""Config flow to configure the GDACS integration."""
+"""Config flow to configure the InMet integration."""
 
 import logging
 from typing import Any
@@ -16,11 +16,9 @@ from homeassistant.const import (
 )
 from homeassistant.helpers import config_validation as cv
 
-from .const import DEFAULT_CODE, DEFAULT_SCAN_INTERVAL, DOMAIN
+from .const import DEFAULT_NAME, DEFAULT_SCAN_INTERVAL, DOMAIN, MAX_CITIES
 
-DATA_SCHEMA = vol.Schema(
-    {vol.Optional(CONF_CODE, default=DEFAULT_CODE): cv.positive_int}
-)
+DATA_SCHEMA = vol.Schema({vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string})
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -28,12 +26,17 @@ _LOGGER = logging.getLogger(__name__)
 class InmetFlowHandler(ConfigFlow, domain=DOMAIN):
     """Handle a InMet config flow."""
 
+    cities = []
+
     async def _show_form(
-        self, errors: dict[str, str] | None = None
+        self,
+        step_id: str,
+        data_schema: vol.Schema,
+        errors: dict[str, str] | None = None,
     ) -> ConfigFlowResult:
-        """Show the form to the user."""
+        """Show a form to the user."""
         return self.async_show_form(
-            step_id="user", data_schema=DATA_SCHEMA, errors=errors or {}
+            step_id=step_id, data_schema=data_schema, errors=errors or {}
         )
 
     async def async_step_user(
@@ -42,19 +45,82 @@ class InmetFlowHandler(ConfigFlow, domain=DOMAIN):
         """Handle the start of the config flow."""
         _LOGGER.debug("User input: %s", user_input)
         if not user_input:
-            return await self._show_form()
+            return await self._show_form("user", DATA_SCHEMA)
 
-        city_code = user_input.get(CONF_CODE, DEFAULT_CODE)
-        user_input[CONF_CODE] = city_code
+        city_name = user_input.get(CONF_NAME, DEFAULT_NAME)
 
-        details = await self._fetch_city_details(city_code)
-        if details is None:
-            return await self._show_form(errors={"base": "cannot_connect"})
+        # Realiza a busca da cidade e limita o número de resultados ao valor de MAX_CITIES
+        all_cities = await self._search_city(city_name)
+        if all_cities is None:
+            return await self._show_form(
+                "user", DATA_SCHEMA, errors={"base": "cannot_connect"}
+            )
 
-        detail = details[0]
-        user_input[CONF_LATITUDE] = detail["latitude"]
-        user_input[CONF_LONGITUDE] = detail["longitude"]
-        user_input[CONF_NAME] = detail["label"]
+        # Limitar a lista de cidades ao número definido em MAX_CITIES
+        self.cities = all_cities[:MAX_CITIES]
+
+        if len(self.cities) == 1:
+            return await self._create_entry_from_city(self.cities[0])
+
+        # Se houver múltiplas cidades, pedir ao usuário para escolher uma
+        city_names = {
+            city["geocode"]: f"{city['label']} ({city['geocode']})"
+            for city in self.cities
+        }
+
+        # Adicionar uma opção extra para voltar, usando o texto de tradução diretamente
+        city_names["back"] = "Voltar para digitar outro nome de cidade"
+
+        # Use vol.Optional para exibir como radio buttons
+        city_selection_schema = vol.Schema(
+            {vol.Optional("city_code"): vol.In(city_names)}
+        )
+
+        return await self._show_form("select_city", city_selection_schema)
+
+    async def async_step_select_city(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Handle city selection step."""
+        city_code = user_input["city_code"]
+
+        # Verifique se o usuário escolheu "Voltar"
+        if city_code == "back":
+            return await self.async_step_user()
+
+        # Encontre a cidade com o código selecionado
+        city = next(
+            (city for city in self.cities if city["geocode"] == city_code), None
+        )
+
+        if not city:
+            return await self._show_form(
+                "select_city",
+                vol.Schema(
+                    {
+                        vol.Optional("city_code"): vol.In(
+                            {
+                                c["geocode"]: f"{c['label']} ({c['geocode']})"
+                                for c in self.cities
+                            }
+                        )
+                    }
+                ),
+                errors={"base": "city_not_found"},
+            )
+
+        # Crie a entrada de configuração a partir da cidade selecionada
+        return await self._create_entry_from_city(city)
+
+    async def _create_entry_from_city(self, city: dict) -> ConfigFlowResult:
+        """Create config entry from city data."""
+        user_input = {
+            CONF_CODE: city["geocode"],
+            CONF_LATITUDE: city["latitude"],
+            CONF_LONGITUDE: city["longitude"],
+            CONF_NAME: city["label"],
+            CONF_SCAN_INTERVAL: DEFAULT_SCAN_INTERVAL.total_seconds(),
+        }
 
         identifier = f"inmet_{user_input[CONF_CODE]}"
         title = f"InMet Alert {user_input[CONF_NAME]}"
@@ -62,20 +128,15 @@ class InmetFlowHandler(ConfigFlow, domain=DOMAIN):
         await self.async_set_unique_id(identifier)
         self._abort_if_unique_id_configured()
 
-        scan_interval = user_input.get(CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL)
-        user_input[CONF_SCAN_INTERVAL] = scan_interval.total_seconds()
-
         return self.async_create_entry(title=title, data=user_input)
 
-    async def _fetch_city_details(self, city_code: str) -> dict | None:
-        """Fetch city details from external server."""
-        url = f"https://apiprevmet3.inmet.gov.br/buscar/cidade/{city_code}"
+    async def _search_city(self, name: str) -> list | None:
+        """Search the city using the inmet endpoint."""
+        url = f"https://apiprevmet3.inmet.gov.br/autocomplete/{name}"
         try:
             async with aiohttp.ClientSession() as session, session.get(url) as response:
                 if response.status != 200:
-                    _LOGGER.error(
-                        "Failed to fetch details for city code: %s", city_code
-                    )
+                    _LOGGER.error("Failed to search city: %s", name)
                     return None
                 return await response.json()
         except aiohttp.ClientError as err:
